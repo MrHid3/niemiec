b6f18123808712737dab07047f35e62d852b40b7
MrHid3 <gkornas@proton.me>
2024-11-13 09:14:27 +0100

    nie skończyłem, moore w połowie, naiwny zrobiony

--- boyre-moore/main.cpp
+++ boyre-moore/main.cpp
@@ -1,13 +1,58 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
+int estupido(string needle, string haystack) {
+    for(int i = 0; i < haystack.length(); i++) {
+        int checked = 0;
+        for(int j = 0; j < needle.length(); j++) {
+            if(haystack[i + j] == needle[j])
+                checked++;
+            else {
+                break;
+            }
+        }
+        if(checked == needle.length()) return i;
+    }
+    return -1;
+}
+
+int boyreMoore(string needle, string haystack) {
+    int alphabet[256];
+
+    for(int i = 0; i < 255; i++)
+        alphabet[i] = needle.length();
+    for(int i = 0; i < needle.length(); i++)
+        alphabet[(int)needle[i]] = needle[i];
+
+    for(int i = 0; i < 255; i++) {
+        cout << alphabet[i] << endl;
+    }
+
+    int addedIndex;
+    for(int i = needle.length() - 1; i < haystack.length(); i += addedIndex) {
+        if(needle[needle.length() - 1] == haystack[i]) {
+            int checked = 0;
+            for(int j = 1; j < needle.length(); j++) {
+                if(haystack[i - j] == needle[needle.length() - (j + 1)])
+                    checked++;
+                else
+                    break;
+            }
+            if(checked == needle.length())
+                return i;
+        }
+        addedIndex = alphabet[(int)haystack[i]];
+    }
+    return -1;
+}
+
 int main() {
-    cout << "Hello, World!" << endl;
+    string needle = "ips";
+    string haystack = "lorem ipsum";
+    cout << estupido(needle, haystack) << endl;
+    cout << boyreMoore(needle, haystack) << endl;
     return 0;
 }
 
-int estupido(string needle, string haystack) {
-
-    return -1;
-}
