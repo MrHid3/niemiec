c954e6a3041831ae3f0bfbd86880a18c1c2bbd16
Gabriel Kornaś <g_kornas@inf15.local>
2024-11-13 08:37:10 +0100

    Created repo

---
+++ boyre-moore/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+
+int estupido(string needle, string haystack) {
+
+    return -1;
+}
