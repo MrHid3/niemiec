b81fafdb7ec120b8c906031aa4c15f7f8dc49cf9
MrHid3 <gkornas@proton.me>
2024-11-24 18:39:37 +0100

    patryk niemiec ma 500IQ (optymalizacja pół pikosekundy)

--- boyre-moore/boyremoore.cpp
+++ boyre-moore/boyremoore.cpp
@@ -39,15 +39,12 @@ int boyreMoore(string needle, string heystack) {
 
     int addedIndex;
     for(int i = needle.length() - 1; i < heystack.length(); i += addedIndex) {
-        bool checked = false;
-        for(int j = 0; j < needle.length(); j++) {
-            if(heystack[i - j] == needle[needle.length() - j - 1])
-                checked = true;
-            else{
+        bool checked = true;
+        for(int j = 0; j < needle.length(); j++)
+            if(heystack[i - j] != needle[needle.length() - j - 1]){
                 checked = false;
                 break;
             }
-        }
         // cout << "checked: " << checked << endl;
         // cout << "addedIndex: " << alphabet[(int)heystack[i]] << endl;
         // cout << "heystack[i]: " << heystack[i] << endl;
